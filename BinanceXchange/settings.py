4909a98aee3de391b945cdc8524b61f635de16b8
Demch-volodymyr <115727199+Demch-volodymyr@users.noreply.github.com>
2024-03-20 02:49:08 +0200

    Update settings.py to false
    idk

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yz@arq$5207orb+c0+5jto_#9-l==yw^#vhjw$0vz2_!!=@fer'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['binancexchange.azurewebsites.net', '127.0.0.1']
 
