1dcd8b5d7724e455ab2d22b8930ae0284910c029
Demch-volodymyr <115727199+Demch-volodymyr@users.noreply.github.com>
2024-04-07 18:59:41 +0300

    Add files via upload

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -159,8 +159,6 @@ SOCIALACCOUNT_PROVIDERS = {
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-passwrd: str = os.getenv('vqur snto nqrm pavw')
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
