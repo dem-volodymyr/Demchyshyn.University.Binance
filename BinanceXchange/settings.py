7edb077a84ccc061a4aca8d63cc945e81ff50860
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2024-04-08 12:07:27 +0300

    week 5

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'xchangeua@gmail.com'
-EMAIL_HOST_PASSWORD = 'vqursntonqrmpavw'
+EMAIL_HOST_PASSWORD = 'bcqv mmid toxp dxiq'
 EMAIL_USE_TLS = True
