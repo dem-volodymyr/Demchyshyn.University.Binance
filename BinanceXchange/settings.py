1dda5a658f8358ee02d878bfb2ef58abe33a1d0a
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2025-05-27 09:25:32 +0300

    added sonarqube and sonar-scanner, make tests for code,used docker for deploy

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'BinanceXchange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DJANGO_DB_NAME'),
+        'USER': os.environ.get('DJANGO_DB_USER'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD'),
+        'HOST': os.environ.get('DJANGO_DB_HOST'),
+        'PORT': '5432',
     }
 }
 
