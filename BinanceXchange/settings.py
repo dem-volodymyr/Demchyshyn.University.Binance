ba4391db147cf2eb54708698c4bf2ed046d42566
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2025-05-30 01:47:39 +0300

    site id-3

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Site ID
-SITE_ID = 1
+SITE_ID = 3
 
 # Social authentication settings
 SOCIALACCOUNT_LOGIN_ON_GET = True
