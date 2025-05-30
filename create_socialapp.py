b18a4d464e1fa13066d001be9589ac725ecc282e
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2025-05-30 04:53:51 +0300

    rebuild

--- create_socialapp.py
+++ create_socialapp.py
@@ -7,7 +7,14 @@ django.setup()
 from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
 
-site = Site.objects.get_current()
+# Створюємо Site, якщо його немає
+site, created = Site.objects.get_or_create(
+    id=1,
+    defaults={
+        "domain": "demchyshynuniversitybinance-production-25de.up.railway.app",
+        "name": "demchyshynuniversitybinance-production-25de.up.railway.app"
+    }
+)
 
 client_id = os.environ.get('GOOGLE_CLIENT_ID')
 secret = os.environ.get('GOOGLE_SECRET')
