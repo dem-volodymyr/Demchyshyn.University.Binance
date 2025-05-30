51346dd431468bbba5acf574d349420bec266ca0
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2025-05-30 04:20:46 +0300

    rebuild

--- create_socialapp.py
+++ create_socialapp.py
@@ -1,1 +1,25 @@
- 
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BinanceXchange.settings")
+django.setup()
+
+from allauth.socialaccount.models import SocialApp
+from django.contrib.sites.models import Site
+
+site = Site.objects.get_current()
+
+client_id = os.environ.get('GOOGLE_CLIENT_ID')
+secret = os.environ.get('GOOGLE_SECRET')
+
+if not client_id or not secret:
+    raise Exception("GOOGLE_CLIENT_ID or GOOGLE_SECRET not set in environment variables.")
+
+app, created = SocialApp.objects.get_or_create(
+    provider='google',
+    name='Google',
+    client_id=client_id,
+    secret=secret,
+)
+app.sites.add(site)
+print("SocialApp created:", app, "Created:", created)
