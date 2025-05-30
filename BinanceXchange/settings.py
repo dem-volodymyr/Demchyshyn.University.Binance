7c8acfdcfe084f5b0e9d3c701e8edc05a833a8d5
demchyshynvolodymyr <valdemarmalyna@gmail.com>
2025-05-30 03:47:10 +0300

    rebuild

--- BinanceXchange/settings.py
+++ BinanceXchange/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.contrib import staticfiles
 import os
 import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,10 +99,9 @@ WSGI_APPLICATION = 'BinanceXchange.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
